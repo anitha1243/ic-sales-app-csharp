cb2c6b7b53b1a73184f56751249421975ed6a849
anitha1243 <kurian.anithajose90@gmail.com>
2022-10-08 21:49:32 +1100

    first commit

---
+++ Call_Controller_ReactJS_MVC/Controllers/CustomerController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using IC_MVP_Project_Task1.DAL;
+using IC_MVP_Project_Task1.Models;
+using NLog;
+
+namespace IC_MVP_Project_Task1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private SalesContext db = new SalesContext();
+
+        // GET: Customer
+        [HttpGet]
+        public JsonResult Index()
+        {
+            var dataSourceList = new List<Customer>();
+
+            for(int i = 0; i < db.Customers.ToList().Count(); i++)
+            {
+                dataSourceList.Add(new Customer
+                {
+                    Name = db.Customers.ToList()[i].Name,
+                    Address = db.Customers.ToList()[i].Address
+                });
+            }            
+
+            // convert it to array
+            var dataSource = dataSourceList.ToArray();
+            return Json(dataSource, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,Address")] Customer customer)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Customers.Add(customer);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException dex)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customer/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var customerToUpdate = db.Customers.Find(id);
+            if (TryUpdateModel(customerToUpdate, "",
+               new string[] { "Name", "Address" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (DataException dex)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            return View(customerToUpdate);
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                Customer customer = db.Customers.Find(id);
+                db.Customers.Remove(customer);
+                db.SaveChanges();
+            }
+            catch (DataException dex)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Call_Controller_ReactJS_MVC/Scripts/ModalButton.jsx
@@ -0,0 +1,45 @@
+﻿const { Button, Header, Image, Modal, Label, Input } = semanticUIReact
+function ModalButton() {
+    const [open, setOpen] = React.useState(false);
+    const [name, setName] = React.useState("");
+    const [address, setAddress] = React.useState("");
+
+    return (
+        <Modal
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            open={open}
+            trigger={<Button>New Customer</Button>}
+        >
+            <Modal.Header>Create Customer</Modal.Header>
+            <Modal.Content image>
+                
+                <Modal.Description>
+                    <div>
+                        <Label>NAME</Label>
+                        <Input onChange={event => setName(event.target.value)} />
+                    </div>
+                    <div>
+                        <Label>ADDRESS</Label>
+                        <Input onChange={event => setAddress(event.target.value)} />
+                    </div>                   
+                    
+                </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button color='black' onClick={() => setOpen(false)}>
+                    Cancel
+                </Button>
+                <Button
+                    content="Create"
+                    labelPosition='right'
+                    icon='checkmark'
+                    onClick={() => setOpen(false)}
+                    positive
+                />
+            </Modal.Actions>
+        </Modal>
+    )
+}
+
+//export default ModalButton
